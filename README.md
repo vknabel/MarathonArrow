9a3f1692bf0e5ddf77b9211d64fb760d5ddef8c1
Valentin Knabel <dev@vknabel.com>
2018-01-07 12:24:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MarathonArrow
+
+A description of this package.
---
+++ Sources/MarathonArrow/main.swift
@@ -0,0 +1,19 @@
+import ArrowKit
+import MarathonCore
+
+struct MarathonArrow: Arrow {
+    let arrow: String
+    let help: String?
+    let run: String
+    let cachePath: String?
+
+    public func fire(archerfile: Archerfile, arguments: [String]) throws {
+        try Marathon.run(
+            with: ["marathon", "run", run] + arguments,
+            folderPath: cachePath ?? ".archery/marathon",
+            printFunction: { print($0.dropLast()) }
+        )
+    }
+}
+
+MarathonArrow.fire()
